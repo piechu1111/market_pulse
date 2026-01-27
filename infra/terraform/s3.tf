39a78d1baa8f4603b16da7694c120edc6cbaf69a
piechu1111 <tomek.piesniewski@gmail.com>
2026-01-27 11:14:51 +0100

    add dev s3 data lake bucket

---
+++ infra/terraform/s3.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "data_lake" {
+  bucket = "market-pulse-data-eu-central-1-dev"
+
+  tags = local.common_tags
+}
+
+# block all public access
+resource "aws_s3_bucket_public_access_block" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# server side encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# enable versioning
+resource "aws_s3_bucket_versioning" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
